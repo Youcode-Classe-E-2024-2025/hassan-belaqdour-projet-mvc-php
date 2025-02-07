21c7c038ad818b4093f58d571005f3b4b4c2f43c
hassanbelaqdour <hassanbelaqdour@gmail.com>
2025-02-07 16:34:43 +0100

    modifier le code pour quel corresppend au login et registr

--- public/index.php
+++ public/index.php
@@ -1,10 +1,17 @@
 <?php
 
+require_once '../vendor/autoload.php'; // Charger Twig
 require_once '../app/controllers/AuthController.php';
+require_once '../config/database.php';
+
+// Configuration de Twig
+$loader = new \Twig\Loader\FilesystemLoader('../app/views');
+$twig = new \Twig\Environment($loader);
+
+// Créer une instance du contrôleur
+$authController = new AuthController($twig);
 
 $action = isset($_GET['action']) ? $_GET['action'] : '';
-
-$authController = new AuthController();
 
 switch ($action) {
     case 'login':
@@ -14,6 +21,6 @@ switch ($action) {
         $authController->signup();
         break;
     default:
-        include '../app/views/login.php';
+        echo $twig->render('login.twig');
         break;
 }
